e36f3e9e1e928def0f17debc9fab886bd945986a
Your Name <you@example.com>
2025-05-04 19:21:13 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -57,7 +57,7 @@ TreeMap *createTreeMap(int (*lower_than)(void *key1, void *key2))
     return mapa;
 }
 
-PPair *searchTreeMap(TreeMap *tree, void *key)
+Pair *searchTreeMap(TreeMap *tree, void *key)
 {
     TreeNode *aux = tree->root;
 
